df1a8696301af04ad166c37b7fdab83da7ac0699
Joe Ganobsik <jganobsik@gmail.com>
2019-03-21 14:54:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,17 +11,22 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 def create
-  @post = Post.new(params.require(:post).permit(:title, :description))
+  @post = Post.new(post_params)
   @post.save
   redirect_to post_path(@post)
 end
  
 def update
   @post = Post.find(params[:id])
-  @post.update(params.require(:post).permit(:title))
+  @post.update(post_params)
   redirect_to post_path(@post)
 end
+ 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+	
+	
+	
 end
+
